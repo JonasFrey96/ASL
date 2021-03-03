4264371d71fe4193b8a1398794c4f2e793a1a17d
Jonas Frey <frey.breitenbrunn@gmx.de>
2021-03-03 09:43:42 +0000

    remved env setting with exported variable
    Former-commit-id: fa92116d9f30086fc880f5ea1bc1c13ddc796930

--- supervisor.py
+++ supervisor.py
@@ -27,25 +27,23 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()    
   parser.add_argument('--exp', type=file_path, default='cfg/exp/exp.yml',
                       help='The main experiment yaml file.')
-  parser.add_argument('--env', type=file_path, default='cfg/env/env.yml',
-                      help='The environment yaml file.')
   parser.add_argument('--mode', default='module', choices=['shell','module'],
                       help='The environment yaml file.')
   
   args = parser.parse_args()
   exp = load_yaml(args.exp)
-  env = load_yaml(args.env)
+  env_cfg_path = os.path.join('cfg/env', os.environ['ENV_WORKSTATION_NAME']+ '.yml')
+  env = load_yaml(env_cfg_path)
+  
   if exp['max_tasks'] > exp['task_generator']['total_tasks']:
     print('Max Tasks larger then total tasks -> Setting max_tasks to total_tasks')
     exp['max_tasks'] = exp['task_generator']['total_tasks']
-
-  exp = load_yaml(args.exp)
-  env = load_yaml(args.env)
+  
   if args.mode != 'sell':
     sys.path.append(os.path.join(os.getcwd() + '/train_task.py'))
     from train_task import train_task
     from utils import get_neptune_logger
-    logger = get_neptune_logger(exp,env, args.exp, args.env)
+    logger = get_neptune_logger(exp,env, args.exp, env_cfg_path)
     
   for i in range( exp['max_tasks'] ):
     init = int(bool(i==0))
@@ -57,9 +55,9 @@ if __name__ == "__main__":
       else:
         mc = '/cluster/home/jonfrey/miniconda3/envs/track4'
       cmd = f'cd $HOME/ASL && {mc}/bin/python train_task.py' 
-      cmd += f' --exp={args.exp} --env={args.env} --init={init} --task_nr={i} --close={close}'
+      cmd += f' --exp={args.exp} --init={init} --task_nr={i} --close={close}'
       print("Execute script: " , cmd)
       os.system(cmd)
     else:
-      train_task( init, close, args.exp, args.env, i, logger_pass=logger)
+      train_task( init, close, args.exp, env_cfg_path, i, logger_pass=logger)
 
